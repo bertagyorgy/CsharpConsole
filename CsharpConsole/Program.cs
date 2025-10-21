0c72db86d4052d2aab67bd22532fb5f37eb61604
bertagyorgy <bertagyurci14@gmail.com>
2025-10-21 12:22:55 +0200

    new feature

--- CsharpConsole/Program.cs
+++ CsharpConsole/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading.Tasks.Dataflow;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string Szoveg = "bab";
+        Console.WriteLine("Hello, World!");
+    }
+}
